4603460f8ab1201ebae9645f2ca2a44165c07fcf
Eric Crosson <eric.s.crosson@utexas.edu>
2023-05-07 08:54:02 -0500

    build(nix): separate dev only flake inputs with subflake

--- flake.nix
+++ flake.nix
@@ -3,52 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    pre-commit-hooks,
-  }: (
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = forEachSystem (system: {
+      default = nixpkgs.legacyPackages.${system}.writeShellApplication {
         name = "barrier";
-        scriptBuildInputs = with pkgs; [netcat];
-        script = (pkgs.writeScriptBin name (builtins.readFile ./barrier)).overrideAttrs (old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              alejandra.enable = true;
-              prettier.enable = true;
-              shellcheck.enable = true;
-              shfmt.enable = true;
-            };
-          };
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          inherit (checks.pre-commit-check) shellHook;
-        };
-        packages.default = pkgs.symlinkJoin {
-          inherit name;
-          paths = [script] ++ scriptBuildInputs;
-          buildInputs = [pkgs.makeWrapper];
-          postBuild = "wrapProgram $out/bin/${name}";
-        };
-      }
-    )
-  );
+        runtimeInputs = with nixpkgs.legacyPackages.${system}; [netcat];
+        text = builtins.readFile ./barrier;
+      };
+    });
+  };
 }
---
+++ nix/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "barrier dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    devShells = forEachSystem (system: let
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ../.;
+          hooks = {
+            alejandra.enable = true;
+            prettier.enable = true;
+            shellcheck.enable = true;
+            shfmt.enable = true;
+          };
+        };
+      };
+    in {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+      };
+    });
+  };
+}
